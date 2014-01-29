ab1f01e80cd6cb037192b92382288979b819ecd2
David Sánchez <dasaingelectronico@gmail.com>
2014-01-28 23:55:36 -0500

    editar 2 login.php

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
-
 <html>
  <head>
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo '<h1>Hola Mundo</h1>'; ?>
+  <?php echo '<h1>Login</h1>'; ?>
+  <?php echo '<Input name="usuario" id="usuario">'; ?>
+  <?php echo '<Input name="password" id="password">'; ?>
  </body>
 </html>
